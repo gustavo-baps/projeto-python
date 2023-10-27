c9a7b4d3997ae84d6f003b0875d60c718da46019
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-10-27 09:18:28 -0300

    commit 27/10/2023

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
@@ -21,3 +21,25 @@ def store(request):
         data['msg'] = 'Usuário cadastrado'
         data['class'] = 'alert-success'
     return render(request, 'create.html', data)
+
+def painel(request):
+    return render(request, 'painel.html')
+
+def dologin(request):
+    user = authenticate(username=request.POST['user'], password=request.POST['password'])
+    data = {}
+    if user is not None:
+        login(request, user)
+        return redirect('/dashboard/')
+    else:
+        data['msg'] = 'Usuário ou senha inválidos'
+        data['class'] = 'alert-danger'
+        return render(request, 'painel.html', data)
+    
+def dashboard(request):
+    return render(request, 'dashboard/home.html')
+
+def logouts(request):
+    logout(request)
+    return redirect('/painel/')
+
