2304f9861bd9b955c8fbead962b168485afe69a3
Gustavo Baps <gustavobaps1903@gmail.com>
2023-12-03 14:48:13 -0300

    commit

--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -9,6 +9,5 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto.settings")
 application = get_wsgi_application()
-app = application
