20c0fa679a74dfe20256db645e0d539f0d78e1c5
Gustavo Baps <gustavobaps1903@gmail.com>
2023-12-03 16:00:40 -0300

    commit

--- projeto/settings.py
+++ projeto/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,8 +32,8 @@ DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Application definition
 
--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from app.views import home, painel, dologin, dashboard, logouts, clientes, addcliente, deletecliente, editcliente, equipes, addequipe, deleteequipe, editequipe, mecanicos, addmecanico, deletemecanico, editmecanico, carros, addcarro, deletecarro, editcarro, pecas, addpeca, deletepeca, editpeca, servicos, addservico, deleteservico, editservico, ordens, addordem, deleteordem, editordem, ordem, filtercarros, adm
 
 urlpatterns = [
@@ -58,3 +60,5 @@ urlpatterns = [
     path('filtercarros/<int:cliente_id>/', filtercarros, name='filtercarros'),
     path('adm/', adm)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
